e0b78a08bd22d202240b2208a8f313ebd3dd865b
Martin Wetterwald <martin@wetterwald.eu>
2015-09-26 19:05:22 +0200

    Refactors 1st exercise (2015).

--- 2015/src/01.cpp
+++ 2015/src/01.cpp
@@ -1,13 +1,9 @@
 #include <iostream>
-#include <vector>
-int reponse_universelle(int n){
-  return 42;
-}
 
-
-int main(){
-  int n;
-  std::cin >> n >> std::skipws;
-  std::cout << reponse_universelle(n) << "\n";
-  return 0;
+int main()
+{
+    int n;
+    std::cin >> n >> std::skipws;
+    std::cout << 42 << "\n";
+    return 0;
 }
