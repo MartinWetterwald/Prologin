ecd3a4b0a9ef893cc01d2990d711592a835aff61
Martin Wetterwald <martin@wetterwald.eu>
2015-09-27 20:39:17 +0200

    Refactors exercise 6 initial code snippet.

--- 2015/src/06.cpp
+++ 2015/src/06.cpp
@@ -1,37 +1,53 @@
 #include <iostream>
 #include <vector>
-class escargot {
+
+class Snail
+{
 public:
-  int position;
-  int vitesse;
+    int position;
+    int speed;
 };
 
-void course_baveuse0(int n, int c, std::vector<escargot *> * joueur1, std::vector<escargot *> * joueur2){
-  /* Inserez votre code ici */
+void race ( int n, int c, std::vector <Snail *> * player1, std::vector <Snail *> * player2 )
+{
 }
 
+int main ( )
+{
+    int v2, p2, v, p, c, n;
+    std::cin >> n >> std::skipws >> c;
+    std::vector <Snail *> * player1 = new std::vector <Snail *> ( n );
+    for ( int i = 0 ; i < n ; i++ )
+    {
+        std::cin >> p >> std::skipws >> v;
+        Snail * q = new Snail ( );
+        q -> position = p;
+        q -> speed = v;
+        player1 -> at ( i ) = q;
+    }
+    std::vector <Snail *> * player2 = new std::vector <Snail *> ( n );
+    for ( int i2 = 0 ; i2 < n ; ++i2 )
+    {
+        std::cin >> p2 >> std::skipws >> v2;
+        Snail * r = new Snail ( );
+        r -> position = p2;
+        r -> speed = v2;
+        player2 -> at ( i2 ) = r;
+    }
+    race ( n, c, player1, player2 );
 
-int main(){
-  int v2, p2, v, p, c, n;
-  std::cin >> n >> std::skipws >> c;
-  std::vector<escargot * > *joueur1 = new std::vector<escargot *>( n );
-  for (int i = 0 ; i < n; i++)
-  {
-    std::cin >> p >> std::skipws >> v;
-    escargot * q = new escargot();
-    q->position=p;
-    q->vitesse=v;
-    joueur1->at(i) = q;
-  }
-  std::vector<escargot * > *joueur2 = new std::vector<escargot *>( n );
-  for (int i2 = 0 ; i2 < n; i2++)
-  {
-    std::cin >> p2 >> std::skipws >> v2;
-    escargot * r = new escargot();
-    r->position=p2;
-    r->vitesse=v2;
-    joueur2->at(i2) = r;
-  }
-  course_baveuse0(n, c, joueur1, joueur2);
-  return 0;
+    for ( unsigned int i = 0 ; i < player1 -> size ( ) ; ++i )
+    {
+        delete player1 -> at ( i );
+    }
+
+    for ( unsigned int i = 0 ; i < player2 -> size ( ) ; ++i )
+    {
+        delete player2 -> at ( i );
+    }
+
+    delete player1;
+    delete player2;
+
+    return 0;
 }
