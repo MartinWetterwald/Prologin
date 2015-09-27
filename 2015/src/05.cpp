f5bcaeb13bcf05c377cdafe8265cc6290210feea
Martin Wetterwald <martin@wetterwald.eu>
2015-09-27 12:51:21 +0200

    Refactors exercise 5 initial code snippet.

--- 2015/src/05.cpp
+++ 2015/src/05.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
-#include <vector>
-void creneaux0(int n, int h){
-  /* Inserez votre code ici */
+
+void crenellations ( int n, int h )
+{
 }
 
-
-int main(){
-  int h, n;
-  std::cin >> n >> std::skipws >> h;
-  creneaux0(n, h);
-  return 0;
+int main ( )
+{
+    int h, n;
+    std::cin >> n >> std::skipws >> h;
+    crenellations ( n, h );
+    return 0;
 }
