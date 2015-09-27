d8060e4ace32edfa81232ae2c502a918f51ee275
Martin Wetterwald <martin@wetterwald.eu>
2015-09-27 12:47:53 +0200

    Adds initial C++ snippet for exercise 5.

---
+++ 2015/src/05.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+void creneaux0(int n, int h){
+  /* Inserez votre code ici */
+}
+
+
+int main(){
+  int h, n;
+  std::cin >> n >> std::skipws >> h;
+  creneaux0(n, h);
+  return 0;
+}
