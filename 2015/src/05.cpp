15decbdb30bc695779b987c0770ffd0e1371bef8
Martin Wetterwald <martin@wetterwald.eu>
2015-09-27 13:06:15 +0200

    Implements core and bottom of castle

--- 2015/src/05.cpp
+++ 2015/src/05.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <iterator>
 
 void crenellations ( int n, int h )
 {
+    int width = 4 * ( n - 1 ) + 3;
+
+    // Core of the Castle (2 < line < h+1)
+    for ( int line = 3 ; line < h + 1 ; ++line )
+    {
+        std::cout << "|";
+        std::fill_n ( std::ostream_iterator <char> ( std::cout ), width - 2, ' ' );
+        std::cout << "|";
+        std::cout << std::endl;
+    }
+
+    // Last line
+    std::cout << "|";
+    std::fill_n ( std::ostream_iterator <char> ( std::cout ), width - 2, '_' );
+    std::cout << "|";
 }
 
 int main ( )
