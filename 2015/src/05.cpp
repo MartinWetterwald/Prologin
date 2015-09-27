d8026a52a5aafaf96982c7273bbc9fc5c758470b
Martin Wetterwald <martin@wetterwald.eu>
2015-09-27 13:19:47 +0200

    Implements exercise 5 (2015).

--- 2015/src/05.cpp
+++ 2015/src/05.cpp
@@ -5,6 +5,38 @@ void crenellations ( int n, int h )
 {
     int width = 4 * ( n - 1 ) + 3;
 
+    // Top of Castle
+    // First line
+    for ( int c = 1 ; c < width ; ++c )
+    {
+        if ( c % 2 != 0 || c % 4 == 0 )
+        {
+            std::cout << " ";
+        }
+        else
+        {
+            std::cout << "_";
+        }
+    }
+    std::cout << std::endl;
+
+    // Second line
+    for ( int c = 1 ; c <= width ; ++c )
+    {
+        if ( c % 2 != 0 )
+        {
+            std::cout << "|";
+            continue;
+        }
+        if ( c % 4 == 0 )
+        {
+            std::cout << "_";
+            continue;
+        }
+        std::cout << " ";
+    }
+    std::cout << std::endl;
+
     // Core of the Castle (2 < line < h+1)
     for ( int line = 3 ; line < h + 1 ; ++line )
     {
