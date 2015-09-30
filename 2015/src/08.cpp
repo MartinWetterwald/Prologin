ca95fa8eedf5ffead9f46d6d971e64cafd432f36
Martin Wetterwald <martin@wetterwald.eu>
2015-10-01 00:13:21 +0200

    Implements exercise 8 (semi2015).

--- 2015/src/08.cpp
+++ 2015/src/08.cpp
@@ -1,10 +1,55 @@
 #include <iostream>
+#include <set>
+#include <utility>
 
-int liberties ( int height, int width, int x, int y, int * * matrix )
+enum Color { BLACK = 0, WHITE = 1, EMPTY = 2 };
+typedef std::pair <int, int> Intersection;
+typedef std::set <Intersection> IntersectionSet;
+
+int otherColor ( int color )
 {
-    return 0;
+    if ( color == BLACK )
+    {
+        return WHITE;
+    }
+
+    if ( color == WHITE )
+    {
+        return BLACK;
+    }
+
+    return -1;
 }
 
+int liberties ( int * * matrix, IntersectionSet * visited, int height, int width, int y, int x, int color )
+{
+    if ( visited -> find ( std::make_pair ( y, x ) ) != visited -> end ( ) )
+    {
+        return 0;
+    }
+    visited -> insert ( std::make_pair ( y, x ) );
+
+    if ( x < 0 || x >= width || y < 0 || y >= height )
+    {
+        return 0;
+    }
+
+    if ( matrix [ y ] [ x ] == otherColor ( color ) )
+    {
+        return 0;
+    }
+
+    if ( matrix [ y ] [ x ] == EMPTY )
+    {
+        return 1;
+    }
+
+    return
+        liberties ( matrix, visited, height, width, y, x - 1, color ) +
+        liberties ( matrix, visited, height, width, y, x + 1, color ) +
+        liberties ( matrix, visited, height, width, y - 1, x, color ) +
+        liberties ( matrix, visited, height, width, y + 1, x, color );
+}
 
 int main ( )
 {
@@ -24,7 +69,21 @@ int main ( )
         }
         * i = line;
     }
-    std::cout << liberties ( height, width, x, y, matrix );
 
-    return 0;
+    if ( x < 0 || x >= width || y < 0 || y >= height )
+    {
+        return EXIT_FAILURE;
+    }
+
+    int color = matrix [ y ] [ x ];
+    if ( color != BLACK && color != WHITE )
+    {
+        return EXIT_FAILURE;
+    }
+
+    IntersectionSet * visited = new IntersectionSet ( );
+
+    std::cout << liberties ( matrix, visited, height, width, y, x, color );
+
+    return EXIT_SUCCESS;
 }
