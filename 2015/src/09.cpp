0d26f5e788dad484050f18539b9a6a21d7466b89
Martin Wetterwald <martin@wetterwald.eu>
2015-10-01 20:26:19 +0200

    Further enhances memory consumption. Adds some comments.

--- 2015/src/09.cpp
+++ 2015/src/09.cpp
@@ -4,18 +4,22 @@
 typedef std::pair <short, short> Point;
 typedef std::set <Point> PointSet;
 
-int terrain ( int * * matrix, int height, int width, int y, int x, int altitude, PointSet * toVisit )
+int terrain ( char * * matrix, short height, short width, short y, short x, char altitude, PointSet * toVisit )
 {
+    // Out of bounds
     if ( x < 0 || x >= width || y < 0 || y >= height )
     {
         return 0;
     }
 
+    // Already visited
     if ( matrix [ y ] [ x ] == -1 )
     {
         return 0;
     }
 
+    // It's not a point we want to count now so we skip it,
+    // but are interested in it for later recursions (to visit for later).
     if ( matrix [ y ] [ x ] != altitude )
     {
         toVisit -> insert ( std::make_pair ( y, x ) );
@@ -28,6 +32,7 @@ int terrain ( int * * matrix, int height, int width, int y, int x, int altitude,
         toVisit -> erase ( it );
     }
 
+    // We mark the point as visited, increment the total and recurse with neighborhood.
     matrix [ y ] [ x ] = -1;
 
     return 1 +
@@ -39,24 +44,24 @@ int terrain ( int * * matrix, int height, int width, int y, int x, int altitude,
 
 int main ( )
 {
-    int height, width;
+    short height, width;
     std::cin >> height >> std::skipws;
     std::cin >> width >> std::skipws;
 
     PointSet * toVisit = new PointSet ( );
 
-    int * * matrix = new int * [ height ];
-    for ( int i = 0 ; i < height ; ++i )
+    char * * matrix = new char * [ height ];
+    for ( short i = 0 ; i < height ; ++i )
     {
-        int * line = new int [ width ];
-        for ( int j = 0 ; j < width; ++j )
+        char * line = new char [ width ];
+        for ( short j = 0 ; j < width; ++j )
         {
             std::cin >> line [ j ] >> std::skipws;
         }
         matrix [ i ] = line;
     }
 
-    // Kepp the maximum of all chains and relaunch until we visited all points.
+    // Keep the maximum of all chains and relaunch until we visited all points.
     int max = 0;
     while ( max == 0 || toVisit -> size ( ) > 0 )
     {
@@ -74,7 +79,7 @@ int main ( )
 
     delete toVisit;
 
-    for ( int i = 0 ; i < height ; ++i )
+    for ( short i = 0 ; i < height ; ++i )
     {
         delete [ ] matrix [ i ];
     }
