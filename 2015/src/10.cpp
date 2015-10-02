3f6b35ad6261c35774e633e935bc056a8743472e
Martin Wetterwald <martin@wetterwald.eu>
2015-10-02 11:47:02 +0200

    Implements exercise 10 (semi2015).

--- 2015/src/10.cpp
+++ 2015/src/10.cpp
@@ -1,18 +1,41 @@
 #include <iostream>
 #include <list>
 
-typedef std::pair <int, int> Step;
+typedef std::pair <short, int> Step;
 typedef std::list <Step> StepList;
+
+int cooking ( StepList * recipe, short stepId )
+{
+    if ( recipe [ stepId ].size ( ) == 0 )
+    {
+        return 0;
+    }
+
+    int max = 0;
+    for ( auto & step : recipe [ stepId ] )
+    {
+        int time = step.second + cooking ( recipe, step.first );
+        if ( time > max )
+        {
+            max = time;
+        }
+    }
+
+    return max;
+}
 
 int main ( )
 {
-    int n;
+    short n;
     std::cin >> n;
 
     StepList * recipe = new StepList [ n ];
 
-    for ( int i = 0 ; i < n ; ++i )
+    bool * root = new bool [ n ];
+
+    for ( short i = 0 ; i < n ; ++i )
     {
+        root [ i ] = true;
         int m;
         std::cin >> m;
         StepList steps ( m, { 0, 0 } );
@@ -20,8 +43,20 @@ int main ( )
         {
             std::cin >> step.first;
             std::cin >> step.second;
+
+            if ( root [ step.first ] )
+            {
+                root [ step.first ] = false;
+            }
         }
+        recipe [ i ] = steps;
     }
+
+    // Find root
+    short rootId = 0;
+    for ( ; rootId < n && ! root [ rootId ] ; ++rootId );
+
+    std::cout << cooking ( recipe, rootId );
 
     delete [ ] recipe;
 
